98daa5f8276a3eca14db1701a36c66d083439908
Samnun Azfar <samazfar52@gmail.com>
2022-03-08 01:43:08 +0600

    class banaisi

---
+++ Project1/Source.cpp
@@ -0,0 +1,32 @@
+//sf::Event event;
+//while (window.pollEvent(event))
+//{
+//    if (event.type == sf::Event::Closed)
+//    {
+//        window.close();
+//    }
+//}
+//
+//window.clear(sf::Color::Black);
+//rect.setPosition(0, 700);
+//
+//for (int k = 0; k < 200; k++)
+//{
+//    rect.setSize(sf::Vector2f(arr[k], 5));
+//    if (k == j)
+//    {
+//        rect.setFillColor(sf::Color::Green);
+//    }
+//    else if (k == j + 1)
+//    {
+//        rect.setFillColor(sf::Color::Red);
+//    }
+//    else
+//    {
+//        rect.setFillColor(sf::Color::White);
+//    }
+//    window.draw(rect);
+//    rect.move(5, 0);
+//}
+//window.display();
+////Sleep(1);
--- Project1/graph.cpp
+++ Project1/graph.cpp
@@ -1,80 +1,140 @@
 #include <SFML/Graphics.hpp>
 #include<Windows.h>
+#include<iostream>
+#include <dos.h>
 
+class Button {
+public:
+	int buttpos_x, buttpos_y;
+	int buttwidth, buttheight;
+	sf::RectangleShape butt;
+	sf::Texture n,f,p;
+
+	Button(int x , int y , int w , int h , string &Nn , string &Ff , string &Pp )
+	{
+		butt.setSize(sf::Vector2f(w, h));
+		
+		butt.setOrigin(sf::Vector2f(w / 2.0, h / 2.0));
+		butt.setPosition(sf::Vector2f(x, y));
+		buttpos_x = x; buttpos_y = y;
+		buttwidth = w; buttheight = h;
+		n.loadFromFile(Nn);
+		f.loadFromFile(Ff);
+		p.loadFromFile(Pp);
+		butt.setTexture(&n);
+		
+	}
+	int onButton(sf:: RenderWindow &window)
+	{
+		sf::Vector2i mousepos(sf::Mouse::getPosition(window));
+		int mouse_x = mousepos.x;
+		int mouse_y = mousepos.y;
+
+		if ((buttpos_x < mouse_x && buttpos_x + buttwidth > mouse_x) && (buttpos_y < mouse_y && buttpos_y + buttwidth > mouse_y))
+		{
+			butt.setTexture(&f);
+			std::cout << "Button hovering" << endl;
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				return 2;
+				butt.setTexture(&p);
+			}
+			return 1;
+		}
+		else
+		{
+			butt.setTexture(&n);
+			return 0;
+		}
+		
+
+	}
+
+	void setTexture(string &filename)
+	{
+		sf::Texture jama;
+		if (!jama.loadFromFile(filename))
+		{
+			cout << "Failed to load texture";
+		}
+		return;
+		
+	}
+	void drawButton(sf:: RenderWindow &window)
+	{
+		window.draw(butt);
+	}
+};
 using namespace std;
+
+
 int main()
 {
-    sf:: RenderWindow amarwindow;
-    sf::RectangleShape amarline(sf::Vector2f(5.f, 5.f));
-    float c = 50 ,c_x = 1 ;
-    float del=0;
-    
+	int mouse_x, mouse_y;
+	sf::RenderWindow window(sf::VideoMode(1000, 800), "UI");
+	sf::RectangleShape button1, button2;
+	button1.setPosition(450, 375);
+	button1.setSize(sf::Vector2f(100, 50));
+	sf::Vector2f button1pos(button1.getPosition());
+	button2.setPosition(450, 425+5);
+	button2.setSize(sf::Vector2f(100, 50));
+	sf::Vector2f button2pos(button2.getPosition());
 
-    
-    amarwindow.create(sf::VideoMode(800, 600), "jingalala");
-    amarwindow.setVerticalSyncEnabled(true);
-   // amarline.setPosition(400, 300);
-    //amarline.setOrigin(400, 300);
-    amarline.setPosition(0, 300);
-    
+	sf::Texture n, f, p;
 
-    while (amarwindow.isOpen())
-    {
-        sf::Event ghotonas;
-        while (amarwindow.pollEvent(ghotonas))
-        {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            {
-                c += .1;
-                //Sleep(50);
-                break;
-            }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            {
-                c -= .1;
-                //Sleep(50);
-                break;
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-                c_x += .1;
-                //Sleep(50);
-                break;
-            }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            {
-                c_x -= .1;
-                //Sleep(50);
-                break;
-            }
-            if (ghotonas.type == sf::Event::Closed)
-            {
-                amarwindow.close();
-
-            }
-        }
-        
-        amarwindow.clear(sf::Color::Black);
-        //amarline.rotate(1);
-
-        for (float i = 0; i < 40; i+=.003)
-        {
-            float x = i;
-            amarline.setPosition(20*x*c_x,300- c*(x) * sinf(x - del));
-            amarwindow.draw(amarline);
-        }
-        del += 0.2;
-       // Sleep(1);
-        if (del == 360) del == 0;
-        //amarwindow.draw(amarline);
-        
-        amarwindow.display();
-       // Sleep(3);
+	if (!(n.loadFromFile("normal.png") && f.loadFromFile("focused.png") && p.loadFromFile("pressed.png")))
+	{
+		cout << "Load hoynai";
+		return 0;
+	}
 
 
-    }
-    
+	while (window.isOpen())
+	{
+		sf::Event event;
+		
 
-    return 0;
+		while (window.pollEvent(event))
+		{
+			sf::Vector2i mousepos(sf::Mouse::getPosition(window));
+			mouse_x = mousepos.x;
+			mouse_y = mousepos.y;
+		  
+			if ((button1pos.x < mouse_x && button1pos.x + 100 > mouse_x) && (button1pos.y < mouse_y && button1pos.y + 50 > mouse_y))
+			{
+				button1.setTexture(&f);
+				std::cout << "Button 1 hovering" << endl;
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					button1.setTexture(&p);
+					
+				}
+			}
+			else if ((button2pos.x < mouse_x && button2pos.x + 100 > mouse_x) && (button2pos.y < mouse_y && button2pos.y + 50 > mouse_y))
+			{
+				button2.setTexture(&f);
+				std::cout << "Button 2 hovering" << endl;
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					button2.setTexture(&p);
+				}
+			}
+			else if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			else
+			{
+				button1.setTexture(&n);
+				button2.setTexture(&n);
+				std::cout << "Not hovering" << endl;
+
+			}
+			
+		}
+		window.clear(sf::Color::Black);
+		window.draw(button1);
+		window.draw(button2);
+		window.display();
+	}
 }
